41264e29451ff9106bb6c5b1cc70dc145270de65
sidulanaksekolahan <mirfan.duri@gmail.com>
2026-08-14 17:25:50 +0700

    Testing Jenkins CI/CD

--- src/test/java/com/luv2code/springboot/thymeleafdemo/MyTestDemo.java
+++ src/test/java/com/luv2code/springboot/thymeleafdemo/MyTestDemo.java
@@ -3,8 +3,10 @@ package com.luv2code.springboot.thymeleafdemo;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class MyTestDemo {
 
     @Test
